c0c4709e862794eee5ba752810b2d75b7ef77f7b
Gustavo Mejia <gustavomejiafuentes2111@gmail.com>
2024-03-09 15:31:58 -0600

    testimonial slider

--- ACF/flexible-content/cp-slider.php
+++ ACF/flexible-content/cp-slider.php
@@ -7,7 +7,7 @@ $images = $cp->slider_logo_image;
 
     <h2 class="slider__title"><?php echo $title ?></h2>
 
-    <swiper-container id="swiper" class="mySwiper" slides-per-view="4" navigation="true" keyboard="true" loop="true" space-between="30" free-mode="true" autoplay infinite>
+    <swiper-container id="swiper" class="mySwiper" slides-per-view="3" keyboard="true" loop="true" space-between="30" free-mode="true" autoplay infinite>
     <?php if (isset($images)): ?>
         <?php foreach ($images as $item): ?>
             <swiper-slide>
