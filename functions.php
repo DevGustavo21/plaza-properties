0a923c5aae634dc87beb20b6038d9edde28af579
Gustavo Mejia <gustavomejiafuentes2111@gmail.com>
2024-03-12 11:20:27 -0600

    functions comments fixed

--- functions.php
+++ functions.php
@@ -8,30 +8,48 @@
  * @package news_theme
  */
 
-// ●❱ BASIC SETUP
+/**
+ * Basic setup
+ */
 require_once 'includes/base/basic.php';
 
-// |> Widgets
+/**
+ * Widgets
+ */
 include_once('includes/base/widgets.php');
 
-// |> Enqueue scripts and styles.
+/**
+ * Enqueue scripts and styles
+ */
 require_once 'includes/base/scripts-and-styles.php';
 
-//  |> Helpers loop functions
+/**
+ * Helpers loop functions
+ */
 require_once 'includes/features/helpers-loop.php';
 
-// ●❱ ACF
+/**
+ * ACF
+ */
 require_once 'includes/features/acf.php';
 
-// ●❱ CUSTOMIZER
+/**
+ * CUSTOMIZER
+ */
 include_once('includes/features/settings-customizer.php');
 
-// ●❱ IMPROVE: WP MENU NAV
+
+/**
+ * IMPROVE: WP MENU NAV
+ */
 require_once 'includes/features/improve-wp-nav.php';
 
 
+/**
+ * CUSTOMS
+ */
 
-// ——— Disabled Gutenberg Blocks edit only pages
+/** Disabled Gutenberg Blocks edit only pages */
 add_filter( 'use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2 );
 function prefix_disable_gutenberg( $current_status, $post_type ) {
 	if ( 'page' === $post_type ) {
@@ -40,13 +58,13 @@ function prefix_disable_gutenberg( $current_status, $post_type ) {
 	return $current_status;
 }
 
-//Disabled HTML editor
+/** Disabled HTML editor */
 add_action('init', 'my_remove_editor_from_post_type');
 function my_remove_editor_from_post_type() {
     remove_post_type_support( 'page', 'editor' );
 }
 
-//Allow SVG files
+/**Allow SVG files*/
 function enable_svg_upload( $upload_mimes ) {
     $upload_mimes['svg'] = 'image/svg+xml';
     $upload_mimes['svgz'] = 'image/svg+xml';
@@ -55,17 +73,17 @@ function enable_svg_upload( $upload_mimes ) {
 add_filter( 'upload_mimes', 'enable_svg_upload', 10, 1 );
 
 
-// Calling menu.js file 
+/**Calling menu.js file*/
 function menu() {
     wp_enqueue_script( 'menu_script', get_stylesheet_directory_uri() . '/assets/js/menu.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'menu' );
 
 
-//USE BLOCK EDITOR ONLY FOR PAGE AND GUTENBERG FOR POSTS
+/**USE BLOCK EDITOR ONLY FOR PAGE AND GUTENBERG FOR POSTS */
 add_filter( 'use_block_editor_for_page', '__return_false', 10 );
 
-//ALLOW USE ACF BLOCKS IN GUTENBERG FOR POSTS
+/**ALLOW USE ACF BLOCKS IN GUTENBERG FOR POSTS*/
 add_action('acf/init', function(){
     if (function_exists('acf_register_block_type')){
         acf_register_block_type(array(
