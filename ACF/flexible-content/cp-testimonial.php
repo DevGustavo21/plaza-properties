c6b2d003c4d81a2c54b7404dd4a0707bb78ac876
Erving Bonilla <ervingjbonilla@gmail.com>
2024-03-13 17:22:15 -0600

    Fixed swiper script

--- ACF/flexible-content/cp-testimonial.php
+++ ACF/flexible-content/cp-testimonial.php
@@ -9,64 +9,64 @@ $cta = $cp->call_to_action;
 
     <div class="testimonial__items">
         <?php if (isset($testimonials)): ?>
-                <div class="swiper">
-                    <div class="swiper-wrapper">
-                        <?php foreach ($testimonials as $item): ?>
-                                <div class="swiper-slide">
-                                    <div class="stars">
-                                        <?php for ($i = 1; $i <= $item['stars']; $i++): ?>
-                                                <span class="star <?php echo ($i <= $item['stars']) ? 'filled' : ''; ?>">★</span>
-                                        <?php endfor ?>
-                                    </div>
-                                    <p class="content"><?php echo $item['testimonial-content'] ?></p>
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    <?php foreach ($testimonials as $item): ?>
+                        <div class="swiper-slide">
+                            <div class="stars">
+                                <?php for ($i = 1; $i <= $item['stars']; $i++): ?>
+                                    <span class="star <?php echo ($i <= $item['stars']) ? 'filled' : ''; ?>">★</span>
+                                <?php endfor ?>
+                            </div>
+                            <p class="content"><?php echo $item['testimonial-content'] ?></p>
 
-                                    <div class="extra-info">
-                                        <span class="author"><?php echo $item['author'] ?></span>
-                                        <span class="city"><?php echo $item['city'] ?></span>
-                                    </div>
-                                </div>
-                        <?php endforeach ?>
-                    </div>
+                            <div class="extra-info">
+                                <span class="author"><?php echo $item['author'] ?></span>
+                                <span class="city"><?php echo $item['city'] ?></span>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                </div>
 
-                    <div class="swiper-button-prev"><</div>
-                    <div class="swiper-button-next">></div>
-                </div>  
+                <div class="swiper-button-prev"><</div>
+                <div class="swiper-button-next">></div>
+            </div>
         <?php endif ?>
     </div>
 </section>
 
+<script type="module">
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        freeMode: false,
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: true,
+        infinite: true,
 
-<script>
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-  freeMode: false,
-  slidesPerView: 1,
-  spaceBetween: 10,
-  navigation: true,
-  infinite: true,
-  
-    breakpoints: {
-        768:{
-            slidesPerView: 2,
-            spaceBetween: 10,
+        breakpoints: {
+            768:{
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+
+            870:{
+                slidesPerView: 3,
+            },
+
+            1024:{
+                slidesPerView: 4,
+            },
         },
 
-        870:{
-            slidesPerView: 3,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
 
-        1024:{
-            slidesPerView: 4,
-        },
-    },
+    });
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-});
 </script>
 
